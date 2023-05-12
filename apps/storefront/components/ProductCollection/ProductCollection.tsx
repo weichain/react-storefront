bb02762441822685f2df62560161139f44b6356d
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2023-05-12 17:20:52 +0300

    fix: issues checkout

--- apps/storefront/components/ProductCollection/ProductCollection.tsx
+++ apps/storefront/components/ProductCollection/ProductCollection.tsx
@@ -4,19 +4,19 @@ import { useIntl } from "react-intl";
 import { mapEdgesToItems } from "@/lib/maps";
 import {
   OrderDirection,
+  ProductCollectionQuery,
   ProductCollectionQueryVariables,
   ProductFilterInput,
   ProductOrderField,
   useProductCollectionQuery,
 } from "@/saleor/api";
 import { Text } from "@saleor/ui-kit";
-
+import { ProductCard } from "../ProductCard";
 import { Pagination } from "../Pagination";
 import { useRegions } from "../RegionsProvider";
 import { Spinner } from "../Spinner";
 import { messages } from "../translations";
-import Carousel from "../Carousel/Carousel";
-
+import invariant from "ts-invariant";
 export interface ProductCollectionProps {
   filter?: ProductFilterInput;
   sortBy?: {
@@ -26,6 +26,12 @@ export interface ProductCollectionProps {
   allowMore?: boolean;
   perPage?: number;
   setCounter?: (value: number) => void;
+  gridItems: number;
+}
+
+interface IGridVariants {
+  3: string;
+  4: string;
 }
 
 export function ProductCollection({
@@ -33,7 +39,8 @@ export function ProductCollection({
   sortBy,
   setCounter,
   allowMore = true,
-  perPage = 10,
+  perPage = 3,
+  gridItems,
 }: ProductCollectionProps) {
   const t = useIntl();
   const { query } = useRegions();
@@ -49,6 +56,11 @@ export function ProductCollection({
           field: sortBy.field,
         },
       }),
+  };
+
+  const gridVariants: IGridVariants = {
+    3: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9",
+    4: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-9",
   };
 
   const { loading, error, data, fetchMore } = useProductCollectionQuery({
@@ -83,7 +95,14 @@ export function ProductCollection({
 
   return (
     <div className="w-11/12 m-auto">
-      <Carousel products={products} />
+      <ul
+        className={`${gridVariants[gridItems as keyof IGridVariants]}`}
+        data-testid="productsList"
+      >
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </ul>
       {allowMore && (
         <Pagination
           onLoadMore={onLoadMore}
---
+++ apps/storefront/pages/[channel]/[locale]/order/[email]/index.tsx
@@ -0,0 +1,44 @@
+// import { CheckIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import Image from "next/Image";
+import React, { ReactElement } from "react";
+
+import { Layout } from "@/components";
+import { usePaths } from "@/lib/paths";
+import { useRouter } from "next/router";
+
+function OrderCompletedPage() {
+  const paths = usePaths();
+  const router = useRouter();
+
+  return (
+    <main className="flex items-center justify-center text-center">
+      <div className="flex flex-col items-center my-[81.2px]">
+        <Image src="/checkfilled.svg" alt="checklogo" width={50} height={50} />
+        <p className="font-bold text-[32px] text-[#1E1E1E] mt-2.5 mb-4">Order Complete!</p>
+        <p className="text-[14px] text-[#8F8F8F] w-11/12">
+          We’ve sent an e-mail to <span>{router.query.email}</span>. Please open it and follow the
+          instructions{" "}
+        </p>
+        <button type="button" className="bg-[#E7C130] text-[#1F1F1F] w-full h-10 text-[14px] mt-12">
+          <Link href={paths.$url()}>Continue Browsing</Link>
+        </button>
+      </div>
+    </main>
+  );
+  // return(
+  //   <main className="container pt-8 px-8">
+  //   <CheckIcon className="text-green-700" />
+  //   <div className="font-semibold text-3xl">Your order is completed!</div>
+  //   <p className="mt-2">
+  //     <Link href={paths.$url()}>Go back to homepage</Link>
+  //   </p>
+  //   </main>
+  // )
+}
+
+export default OrderCompletedPage;
+
+OrderCompletedPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
