637080456fcbd9f873da526a8aa5fb60cf843b8f
Steliyan Nedev <steliyannedev@Steliyans-MacBook-Pro.local>
2023-06-13 17:43:34 +0300

    fix regarding order summary pr

--- apps/storefront/pages/[channel]/[locale]/order/[email]/index.tsx
+++ apps/storefront/pages/[channel]/[locale]/order/[email]/index.tsx
@@ -3,13 +3,22 @@ import Link from "next/link";
 import Image from "next/Image";
 import React, { ReactElement } from "react";
 
-import { Layout } from "@/components";
+import { Layout, Spinner } from "@/components";
 import { usePaths } from "@/lib/paths";
 import { useRouter } from "next/router";
+import { useOrderQuery } from "@/saleor/api";
 
 function OrderCompletedPage() {
   const paths = usePaths();
   const router = useRouter();
+  const id = router.query.email as string;
+  const { data, loading } = useOrderQuery({
+    variables: { id: id },
+  });
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <main className="flex items-center justify-center text-center">
@@ -17,8 +26,8 @@ function OrderCompletedPage() {
         <Image src="/checkfilled.svg" alt="checklogo" width={50} height={50} />
         <p className="font-bold text-[32px] text-black mt-2.5 mb-4">Order Complete!</p>
         <p className="text-[14px] text-secondary w-11/12">
-          We’ve sent an e-mail to <span>{router.query.email}</span>. Please open it and follow the
-          instructions{" "}
+          We’ve sent an e-mail to <span>{data?.order?.userEmail}</span>. Please open it and follow
+          the instructions{" "}
         </p>
         <button type="button" className="bg-[#E7C130] text-[#1F1F1F] w-full h-10 text-[14px] mt-12">
           <Link href={paths.$url()}>Continue Browsing</Link>
